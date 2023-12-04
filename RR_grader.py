e0f5c8dc3db227918ab2b28d6629d0cf0f8d9e3e
Stas Kowalski <kowalski.analytics@gmail.com>
2023-12-04 14:47:46 +0100

    added arbitrary categories

--- RR_grader.py
+++ RR_grader.py
@@ -7,6 +7,13 @@ files = [file for file in os.listdir() if file.endswith('.html')]
 # create a set of student names
 students = set()
 
+# filter non-names
+def is_name(name):
+    number  = all(chr == 'I' for chr in name)
+    pronoun = '/' in name
+    suffix  = name == 'Jr.' or name == 'Sr.'
+    return not(number or pronoun or suffix)
+
 # open the list of student names (txt file copied and pasted from the People tab of canvas)
 with open('students.txt', 'r') as file:
     for line in file:
@@ -21,35 +28,53 @@ with open('students.txt', 'r') as file:
             and not any(chr.isdigit() for chr in line) ):
 
             # split the line into a list of words separated by spaces
-            name = line.split()
+            names = line.split()
 
-            # the first and last word in the name line will be used (assuming the last one is not a pronoun '(He/Him)' or a suffix 'I')
-            students.add(name[0] + ' ' + (name[-1] if len(name[-1]) > 1 and '/' not in name[-1] else name[-2]))
+            # the first and last words that are names will be used
+            names = list(filter(is_name,names))
+            students.add(names[0] + ' ' + names[-1])
 
 # alphabetize by last name
+def lastname(name):
+    return name.split()[-1] # last word in name
+
 students = list(students)
-students.sort(key=lambda name: name.split()[-1])
+students.sort(key=lastname) # sort using the function lastname (default sort for strings is alphabetical)
 
-# create a mapping of student names to a list of whether or not they did each reading response
-RR = {}
-ACD = {}
-for student in students:
-    RR[student] = []
-    ACD[student] = []
 
-# for each reading
-for file in files:
+# a datatype for discussion with a types, aliases, a list of dates, and student responses for them.
+class Discussion:
+    def __init__(self, abbr, aliases, dates, response):
+        self.abbr     = abbr
+        self.aliases  = aliases
+        self.dates    = dates
+        self.response = response
+        self.db       = None
 
-    # import the raw html as a readable text document...
-    with open(file, 'r', encoding='utf8') as data:
-        data = data.read()
+    def matches_alias(self, filename):
+        return any((alias in filename) for alias in self.aliases)
 
-        # pick type of discussion post based on file title
-        response = RR if 'Reading' in file else ACD
+discs = []
+with open('categories.txt', 'r') as file:
+    for line in file:
 
-        # and scan the text for each student's name appearing in an author tag on a discussion post.
+        # letters before the colon are used as the discussion type abbreviation
+        abbr, aliases = line.split(':')
+
+        # the abbreviation & any words after the colon are interpreted as an acceptable alias
+        aliases = [abbr] + aliases.split()
+
+        # create a mapping of student names to a list of whether or not they did each reading response
+        response = {}
         for student in students:
-            response[student].append('X' if 'title="Author&#39;s name">' + student.split()[0] in data and student.split()[-1] in data else '')
+            response[student] = []
+
+        # create a list for the dates of discussions of this type
+        dates = []
+
+        # create a discussion of this new type
+        discs.append(Discussion(abbr, aliases, dates, response))
+
 
 # be able to pick out the date from the filename assuming it appears in the format '... Sep 12 ...'
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -60,39 +85,48 @@ def Date(file):
     file = file[i:i+8]
     return file[:3] + ' ' +  ''.join(c for c in file if c.isdigit())
 
-# abbreviate the names of the readings
-readings = ['RR ' + Date(file) for file in files if 'Reading' in file or 'RR' in file]
-discussions = ['ACD ' + Date(file) for file in files if 'After' in file or 'ACD' in file]
+
+# for each reading
+for file in files:
+
+    # import the raw html as a readable text document...
+    with open(file, 'r', encoding='utf8') as data:
+        data = data.read()
+
+        # first type of discussion that matches the aliases in the filename
+        disc = next(disc for disc in discs if disc.matches_alias(file))
+
+        disc.dates.append(Date(file))
+
+        # and scan the text for each student's name appearing in an author tag on a discussion post.
+        for student in students:
+            disc.response[student].append('X' if 'title="Author&#39;s name">' + student.split()[0] in data and student.split()[-1] in data else '')
 
 # sum 'em all up
-readings.append('Total')
-discussions.append('Total')
+for disc in discs:
+    disc.dates.append('Total')
 
-for student in students:
-    RR[student].append(sum(1 for r in RR[student] if r))
-    ACD[student].append(sum(1 for r in ACD[student] if r))
+    for student in students:
+        # add up all the responses for the student that aren't blank 
+        # ("if r" returns False if r is blank, otherwise True)
+        disc.response[student].append(sum(1 for r in disc.response[student] if r))
 
 # put them into a spreadsheet-like format
-rr = pd.DataFrame(RR.values(), index=RR.keys(), columns=readings)
-acd = pd.DataFrame(ACD.values(), index=ACD.keys(), columns=discussions)
-
-print(rr)
-print(acd)
-
-# shorten the column names for Excel
-rr.columns = [read[3:] for read in readings[:-1]] + ['Total']
-acd.columns = [disc[4:] for disc in discussions[:-1]] + ['Total']
+for disc in discs:
+    disc.db = pd.DataFrame(disc.response.values(), index=disc.response.keys(), columns=disc.dates)
+    print(disc.db)
 
 # save as xlsx file
 with pd.ExcelWriter('discussion_posts.xlsx') as writer:
-    rr.to_excel(writer, sheet_name='Reading Responses')
-    acd.to_excel(writer, sheet_name='After Class Discussions')
+    for disc in discs:
+        # make a new sheet within the xlsx file titled with all the aliases of the response type
+        disc.db.to_excel(writer, sheet_name = ' '.join(disc.aliases))
 
 # do some funky stuff to make the Name column wider
 book = openpyxl.load_workbook('discussion_posts.xlsx')
 
-book['Reading Responses'].column_dimensions['A'].width = 20
-book['After Class Discussions'].column_dimensions['A'].width = 20
+for disc in discs:
+    book[' '.join(disc.aliases)].column_dimensions['A'].width = 20
 
 book.save('discussion_posts.xlsx')
 
